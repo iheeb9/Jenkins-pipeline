710498b8e68beffcb5ac86bb90a3bb84eed3b684
iheeb9 <73345856+iheeb9@users.noreply.github.com>
2023-01-19 17:42:17 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,10 +3,11 @@ node{
 	try{
 	def buildNum = env.BUILD_NUMBER
         def branchName= env.BRANCH_NAME
-		print buildNum
-		print branchName
+	
 		stage('PremiereEtape'){
 		  sh "echo 'Hello world !! '"
+			print buildNum
+			print branchName	
  		}
 		stage('DexiemeEtape'){
 		  sh "echo 'Hello world !!'"
