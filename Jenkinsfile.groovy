11eead63c80e77376ef20151ffa53fd08b67c6a7
iheeb9 <73345856+iheeb9@users.noreply.github.com>
2023-01-19 17:38:54 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,10 @@
 node{
     cleanWs()
 	try{
+	def buildNum = env.BUILD_NUMBER
+        def branchName= env.BRANCH_NAME
+		print buildNum
+		print branchName
 		stage('PremiereEtape'){
 		  sh "echo 'Hello world !! '"
  		}
