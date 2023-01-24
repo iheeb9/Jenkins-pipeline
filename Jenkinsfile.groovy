0c55e01034dfc271cbd9bb519aa181d8e9b15622
iheeb9 <73345856+iheeb9@users.noreply.github.com>
2023-01-24 12:05:21 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,14 +1,23 @@
-node{
+node(){
+  try{
+    def buildNum = env.BUILD_NUMBER 
+    def branchName= env.BRANCH_NAME
+    
+    print buildNum
+    print branchName
+
+    stage('Env - clone generator'){
+      git "http://gitlab.example.com/mypipeline/generator.git"
+    }
+
+    stage('Env - run postgres'){
+      sh "./generator.sh -p"
+      sh "docker ps -a"
+      
+    }
+
+  } finally {
+    sh 'docker rm -f postgres'
     cleanWs()
-	try{
-		stage('PremiereEtape'){
-		  sh "echo 'Hello world !! '"
- 		}
-		stage('DexiemeEtape'){
-		  sh "echo 'Hello world !!'"
-		}
-	    }
-	finally{
-		cleanWs()
-		}
-	    }
+  }
+}
