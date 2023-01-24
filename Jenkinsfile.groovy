600e7098e0e834f769c4408b4dee3f1573fb6c6a
iheeb9 <73345856+iheeb9@users.noreply.github.com>
2023-01-24 12:24:02 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,22 +1,23 @@
-node{
+node(){
+  try{
+    def buildNum = env.BUILD_NUMBER 
+    def branchName= env.BRANCH_NAME
+    
+    print buildNum
+    print branchName
+
+    stage('Env - clone generator'){
+    git 'https://github.com/iheeb9/Jenkins-pipeline'
+    }
+
+      stage('Env - run postgres'){
+      sh "./generator.sh -p"
+      sh "docker ps -a"
+      
+    }
+
+  } finally {
+    sh 'docker rm -f postgres'
     cleanWs()
-	try{
-	def buildNum = env.BUILD_NUMBER
-        def branchName= env.BRANCH_NAME
-		print buildNum
-		print branchName
-		
-	stage('POSTGRESQL - Container run')
-		{
-        	sh "./generator.sh -p"
-        	sh "docker ps -a"         
-		}
-	
-
-	   }
-	finally{
-		sh 'docker rm -f postgres'
-        	cleanWs()
-
-  	}
+  }
 }
